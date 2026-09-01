0a3d269d590d72e2c82a3b4654393d08cc0353f5
mtclaremontcommunity <300034651+mtclaremontcommunity@users.noreply.github.com>
2026-09-01 12:48:57 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -74,9 +74,6 @@ function injectMobilePetitionBar() {
   var bar = document.createElement('div');
   bar.className = 'mobile-petition-bar';
   bar.innerHTML = '<a class="mpb-primary" href="' + PETITION_URL + '" target="_blank" rel="noopener">Sign the petition</a>';
-  if (!document.body.hasAttribute('data-no-register-cta')) {
-    bar.innerHTML += '<a class="mpb-register" href="join-us.html#register">+ Register support</a>';
-  }
   document.body.appendChild(bar);
 }
 
